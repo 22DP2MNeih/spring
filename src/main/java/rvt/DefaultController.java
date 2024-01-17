1358f4f6d6dbbf027f337d8c392575c8bd170421
22DP2MNeih <131154525+22DP2MNeih@users.noreply.github.com>
2024-01-17 09:11:11 +0000

    pievienoju test lapu

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -4,17 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DefaultController {
     
-   @GetMapping(value = "/")
-    String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
-        return "index";
-    }
+//    @GetMapping(value = "/")
+//     String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
+//         return "index";
+//     }
 
-    @GetMapping(value = "/abc")
-    String index2(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
-        return "abc";
+//     @GetMapping(value = "/abc")
+//     String index2(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
+//         return "abc";
+//     }
+
+    @GetMapping(value = "/test")
+    ModelAndView testAction() {
+        ModelAndView modelAndView = new ModelAndView("/test");
+        return modelAndView;
     }
 }
